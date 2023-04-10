ac8d080fa9748f73e1ed5cefcbc82e4407443fbd
tt8296065 <85492456+tt8296065@users.noreply.github.com>
2023-04-10 11:18:54 +0800

    Update x-ui-install.sh

--- x-ui-install.sh
+++ x-ui-install.sh
@@ -9,104 +9,13 @@ download_x-ui() {
   DOWNLOAD_LINK="https://https://github.com/tt8296065/x-ui-tt/releases/download/1/x-ui-linux-arm64.tar.gz"
   wget $DOWNLOAD_LINK
 }
-
-install_v2() {
-    unzip -d v2 v2ray-linux-64.zip
-    `mv v2/v2ray /usr/bin`
-    `rm -r v2`
-    `mkdir -p /etc/v2ray`
-    echo '{
-  "log": {
-    "access": "",
-    "error": "",
-    "loglevel": "warning"
-  },
-  "inbounds": [
-    {
-      "port": 36955,
-      "protocol": "vless",
-      "settings": {
-        "udp": false,
-        "clients": [
-          {
-            "id": "bc5995fe-afbc-48c6-804d-d999071bf1ac",
-            "alterId": 0,
-            "email": "t@t.tt",
-            "flow": ""
-          }
-        ],
-        "decryption": "none",
-        "allowTransparent": false
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "/"
-        }
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    },
-    {
-      "tag": "block",
-      "protocol": "blackhole",
-      "settings": {}
-    }
-  ],
-  "routing": {
-    "domainStrategy": "IPIfNonMatch",
-    "rules": []
-  }
-}' > '/etc/v2ray/vless.json'
-}
-
-register_systemd() {
-    echo "#register_system_service and optimized_network edit: 20220428
-[Unit]
-Description=gost
-After=network-online.target
-Wants=network-online.target systemd-networkd-wait-online.service
-
-[Service]
-Type=simple
-User=root
-DynamicUser=true
-ExecStart=/usr/bin/v2ray -c /etc/v2ray/%i.json
-LimitCPU=infinity
-LimitFSIZE=infinity
-LimitDATA=infinity
-LimitSTACK=infinity
-LimitCORE=infinity
-LimitRSS=infinity
-LimitNOFILE=infinity
-LimitAS=infinity
-LimitNPROC=infinity
-LimitMEMLOCK=infinity
-LimitLOCKS=infinity
-LimitSIGPENDING=infinity
-LimitMSGQUEUE=infinity
-LimitRTPRIO=infinity
-LimitRTTIME=infinity
-Restart=always
-RestartSec=20
-
-[Install]
-WantedBy=multi-user.target" > '/etc/systemd/system/v2ray@.service'
-`chmod 777 /etc/systemd/system/v2ray@.service`
-}
-
-main() {
-    download_v2ray
-    install_v2
-    register_systemd
-    `systemctl daemon-reload`
-    `systemctl start v2ray@vless`
-    `systemctl enable v2ray@vless`
-    systemctl status v2ray@vless
-    `rm install.sh v2ray-linux-64.zip`
-}
-
-main
+cd /root/
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
