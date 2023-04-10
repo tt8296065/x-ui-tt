95ca36649eb74ec4ac8fce7b47e5080c1bd07c81
tt8296065 <85492456+tt8296065@users.noreply.github.com>
2023-04-10 11:27:52 +0800

    Update x-ui-install.sh

--- x-ui-install.sh
+++ x-ui-install.sh
@@ -9,13 +9,13 @@ download_x-ui() {
   DOWNLOAD_LINK="https://https://github.com/tt8296065/x-ui-tt/releases/download/1/x-ui-linux-arm64.tar.gz"
   wget $DOWNLOAD_LINK
 }
-cd /root/
-rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
-tar zxvf x-ui-linux-amd64.tar.gz
-chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
-cp x-ui/x-ui.sh /usr/bin/x-ui
-cp -f x-ui/x-ui.service /etc/systemd/system/
-mv x-ui/ /usr/local/
-systemctl daemon-reload
-systemctl enable x-ui
-systemctl restart x-ui
+  cd /root/
+  rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+  tar zxvf x-ui-linux-amd64.tar.gz
+  chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+  cp x-ui/x-ui.sh /usr/bin/x-ui
+  cp -f x-ui/x-ui.service /etc/systemd/system/
+  mv x-ui/ /usr/local/
+  systemctl daemon-reload
+  systemctl enable x-ui
+  systemctl restart x-ui
